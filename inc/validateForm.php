dac294275918471e4382eaccdf938caf372ad3ac
Elizabeth Kodjo <ellizakodjo@outlook.com>
2024-07-28 13:26:58 +0100

    Fixed logo on firefox and contact form messages

--- inc/validateForm.php
+++ inc/validateForm.php
@@ -1,28 +1,20 @@
 <?php
 
 session_start();
+require_once "dbconnect.php";
+$smtp_connection = parse_ini_file(__DIR__ . "/../portfolio.env");
 
-// require "../phpmailer/vendor/autoload.php";
+// create variables for connection 
+// $smtp_host = $smtp_connection["SMTP_HOST"];
+// $smtp_username = $smtp_connection["SMTP_USERNAME"];
+// $smtp_password = $smtp_connection["SMTP_PASSWORD"];
 
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\SMTP;
+$phpmailer_host = $smtp_connection["PHPMAILER_Host"];
+$phpmailer_smtpAuth = $smtp_connection["PHPMAILER_SMTPAuth"];
+$phpmailer_port = $smtp_connection["PHPMAILER_Port"];
+$phpmailer_username = $smtp_connection["PHPMAILER_Username"];
+$phpmailer_password = $smtp_connection["PHPMAILER_Password"];
 
- 
-// $mail = new PHPMailer();
-// $mail->isSMTP();
-// $mail->Host = 'live.smtp.mailtrap.io';
-// $mail->SMTPAuth = true;
-// $mail->Port = 587;
-// $mail->Username = 'api';
-// $mail->Password = '7624ceb5b6c3e28b5c9c6ad764d10a3f';
-
-// $mail->setFrom($email, $firstname);
-// $mail->addAddress("ellizakodjo@outlook.com", "Elizabeth");
-
-// $mail->Subject = "Enquiries";
-// $mail->Body = $message;
-
-// $mail->send();
 
 // Define variables for form data
 $firstname = $lastname = $email = $phone = $message = $token = "";
@@ -31,9 +23,9 @@ $errors = [];
 $data = [];
 
 
-
 // Sanitise incoming data
-function sanitiseIncoming($data) {
+function sanitiseIncoming($data)
+{
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
@@ -50,83 +42,86 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST["message"];
     $submit = $_POST["submit"];
 
-      // Sanitize incoming data
-      $firstname = filter_input(INPUT_POST, 'fname', FILTER_SANITIZE_STRING);
-      $lastname = filter_input(INPUT_POST, 'lname', FILTER_SANITIZE_STRING);
-      $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-      $phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
-      $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+    // Sanitize incoming data
+    $firstname = filter_input(INPUT_POST, 'fname', FILTER_SANITIZE_STRING);
+    $lastname = filter_input(INPUT_POST, 'lname', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
+    $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
 
- if (!empty($firstname)) {
-    $firstname = sanitiseIncoming($firstname);
-    if (!preg_match("/^[A-z-' ]*$/", $firstname)) {
-         $errors[] = "Please enter a valid first name";
-    } 
- } else {
-    $errors[] = "First name field cannot be empty.";
-}
-
- if (!empty($lastname)) {
-    $lastname = sanitiseIncoming($lastname);
-    if (!preg_match("/^[A-z-' ]*$/", $lastname)) {
-        $errors[] = "Please enter a valid last name";
-    } 
- } else {
-    $errors[] = "Last name field cannot be empty.";
-}
-
- if (!empty($email)) {
-    $email = sanitiseIncoming($email);
-    if (filter_var($email, FILTER_VALIDATE_EMAIL) !== $email) {
-        $errors[] = "Email is not valid";
-    } 
- } else {
-    $errors[] = "Email cannot be empty";
- }
-
- if (!empty($phone)) {
-    $phone = sanitiseIncoming($phone);
-    if (!preg_match("/^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/", $phone)) {
-        $errors[] = "Please enter a valid phone number";
+    if (!empty($firstname)) {
+        $firstname = sanitiseIncoming($firstname);
+        if (!preg_match("/^[A-z-' ]*$/", $firstname)) {
+            $errors[] = "Please enter a valid first name";
+        }
+    } else {
+        $errors[] = "First name field cannot be empty.";
     }
 
- } else {
-    $errors[] = "Please enter a phone number.";
- }
+    if (!empty($lastname)) {
+        $lastname = sanitiseIncoming($lastname);
+        if (!preg_match("/^[A-z-' ]*$/", $lastname)) {
+            $errors[] = "Please enter a valid last name";
+        }
+    } else {
+        $errors[] = "Last name field cannot be empty.";
+    }
 
- if (!empty($message)) {
-    $message = sanitiseIncoming($message);
-    //$message = htmlentities($message, ENT_QUOTES, "UTF-8");
- } else {
-    $errors[] = "Please leave a message.";
- }
+    if (!empty($email)) {
+        $email = sanitiseIncoming($email);
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) !== $email) {
+            $errors[] = "Email is not valid";
+        }
+    } else {
+        $errors[] = "Email cannot be empty";
+    }
 
- if ($errors) {
-    $_SESSION['status'] = 'error';  
-    $_SESSION['errors'] = $errors;
-    header('Location: ../index.php?result=error#contact');
-    die();
- } else {
-   
-    $data = [
-        "fname" => $firstname,
-        "lname" => $lastname,
-        "email" => $email,
-        "phone" => $phone,
-        "message" => $message,
-    ];
+    if (!empty($phone)) {
+        $phone = sanitiseIncoming($phone);
+        if (!preg_match("/^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/", $phone)) {
+            $errors[] = "Please enter a valid phone number";
+        }
 
-    $_SESSION['status'] = 'success';
-    $_SESSION['data'] = $data;
-    header('Location: ../index.php#contact?result=success');
-   // die();
-  
- }   
+    } else {
+        $errors[] = "Please enter a phone number.";
+    }
+
+    if (!empty($message)) {
+        $message = sanitiseIncoming($message);
+        //$message = htmlentities($message, ENT_QUOTES, "UTF-8");
+    } else {
+        $errors[] = "Please leave a message.";
+    }
+
+    if ($errors) {
+        $_SESSION['status'] = 'error';
+        $_SESSION['errors'] = $errors;
+        header('Location: ../index.php?result=error#contact');
+        die();
+    } else {
 
 
-try {
-        require_once "dbconnect.php"; 
-        
+
+        $data = [
+            "fname" => $firstname,
+            "lname" => $lastname,
+            "email" => $email,
+            "phone" => $phone,
+            "message" => $message,
+        ];
+
+        $_SESSION['status'] = 'success';
+        $_SESSION['data'] = $data;
+        header('Location: ../index.php#contact?result=success');
+        // die();
+
+    }
+
+
+
+    try {
+
+
         // Sending query to database
         $stmt = $db->prepare("INSERT INTO portfolio (firstname, lastname, email, phone, message)
         VALUES(:firstname, :lastname, :email, :phone, :message)");
@@ -136,20 +131,45 @@ try {
         $stmt->bindParam(':email', $email, PDO::PARAM_STR);
         $stmt->bindParam(':phone', $phone, PDO::PARAM_STR);
         $stmt->bindParam(':message', $message, PDO::PARAM_STR);
-    
+
         $stmt->execute();
-        
+
         header("Location: ../index.php#contact");
 
-       die();
-        
-        } catch (PDOException $e) {
-            echo "Query failed: " . $e->getMessage();
-        }
-    } else {
-        header("Location: ../index.php#contact");
-        exit;
+        die();
+    } catch (PDOException $e) {
+        echo "Query failed: " . $e->getMessage();
     }
+} else {
+    header("Location: ../index.php#contact");
+    exit;
+}
 
+require "../phpmailer/vendor/autoload.php";
 
- 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$toEmail = '$smtp_username';
+
+try {
+    $phpmailer = new PHPMailer();
+    $phpmailer->isSMTP();
+    $phpmailer->Host = $phpmailer_host;
+    $phpmailer->SMTPAuth = $phpmailer_smtpAuth;
+    $phpmailer->Port = $phpmailer_port;
+    $phpmailer->Username = $phpmailer_username;
+    $phpmailer->Password = $phpmailer_password;
+
+    $mail->setFrom($email);
+    $mail->addAddress($toEmail);
+
+    $mail->Subject = "Enquiries";
+    $mail->Body = "<p>Name: {$firstname} </p><p>Last name: {$lastname} </p><p>Email: {$email} </p>
+<p>Phone: {$phone} </p><p> {$message} </p>";
+
+    $mail->send();
+    echo "email sent";
+} catch (Exception $e) {
+    echo "Something went wrong";
+}
--- index.php
+++ index.php
@@ -20,109 +20,106 @@ $firstnameErr = $lastnameErr = $emailErr = $phoneErr = $messageErr = $error_mess
 
 ?>
 
-    <main>
-        <!-- Main Banner -->
-        
-         <?php include "inc/banner.php"; ?>
+<main>
+    <!-- Main Banner -->
 
-        <div class="main-inner">
+    <?php include "inc/banner.php"; ?>
 
-            <!-- My work -->
-            <div class="container mt-3 projects" id="projects">
-                <div class="row myProjects">
-                    <?php include "inc/projects.php"; ?>
-                </div>
-            </div>
-            <!-- Contact details and form -->
-            <div class="container contact" id="contact">
-                <!-- Get in touch -->
-                <div class="col-sm-5 getintouch">
-                    <h1>Get In Touch</h1>
-                    <p>For any further information, please do not hesitate to call or email me using my details below:</p>
-                    <p><span><i class="fa-solid fa-mobile-screen"></i></span><a class="telLink"
-                            href="tel:+447394705450"> 07394 705450</a></p>
-                    <p><span><i class="fa-regular fa-envelope"></i></span><a class="emailLink"
-                            href="mailto:elizabeth.kodjo@yahoo.com"> ellizakodjo@outlook.com</a></p>
-                </div>
-                <!-- Contact Form -->
-                <div class="col-sm-7">
+    <div class="main-inner">
 
-                <?php if(isset($_SESSION['status']) && $_SESSION['status'] === 'error') :
-                $errors = $_SESSION['errors'];
-                ?>
-                <ul class="serverErrors">
-                    
-                    <?php foreach($errors as $e) : ?>
-                        <li><?= $e ?></li>
-                    <?php endforeach; ?>
-                   
-                </ul>
-                    <?php
-                    elseif(isset($_SESSION['status']) && $_SESSION['status'] === 'success') :
-                        $data = $_SESSION['data'];
-                    ?>
-                    <div class="serverSuccess">
-                        <p>Form successfully submitted</p>
-                    </div>
-                    <?php endif; ?>
-
-
-                    
-                    <form class="form" id="form" method="POST" action="inc/validateForm.php">                    
-                        <div class="mb-3 mt-3">
-                            <div class="row formnames">
-                                <div class="col-sm-6 input-control">
-                                    
-                               
-                                    <label for="fname">First Name <small><em>*</em></small></label>
-                                    <input type="text" class="form-control" id="fname" placeholder="First Name*"
-                                        name="fname" value="<?= $firstname ?>">
-                                    <small class="errorMsg"><?=$firstnameErr ?></small> 
-                                    <span class="servererr"><?=$firstnameErr ?></span>                          
-                                    
-                                </div>
-                                <div class="col-sm-6 input-control">
-                                    <label for="lname">Last Name <small>*</small></label>
-                                    <input type="text" class="form-control" id="lname" placeholder="Last Name*"
-                                        name="lname" value="<?=$lastname ?>">
-                                    <small class="errorMsg"><?=$lastnameErr ?></small>
-                                </div>
-                            </div>
-                            <div class="mb-3 input-control">
-                                <label for="email">Email <small>*</small></label>
-                                <input type="text" class="form-control" id="email" placeholder="Email Address"
-                                    name="email" value="<?=$email ?>">
-                                <small class="errorMsg"><?=$emailErr ?></small>
-                            </div>
-                            <div class="mb-3 input-control">
-                                <label for="phone">Phone <small>*</small></label>
-                                <input type="tel" class="form-control" id="phone" placeholder="Phone number"
-                                    name="phone" value="<?= $phone ?>">
-                                <small class="errorMsg"><?=$phoneErr ?></small>
-                            </div>
-                            <div class="mb-3 input-control">
-                                <label for="message">Message <small>*</small></label>
-                                <textarea class="form-control" rows="5" id="message" name="message" value="<?=$message ?>"></textarea>
-                                <small class="errorMsg"><?=$messageErr ?></small>
-                            </div>
-                            <input type="hidden" name="token" value="">
-                            <button type="submit" class="btn btn-primary submitbtn" name="submit">Submit</button>
-                            <p class="errorMsg"></p>
-                            
-                        </div>
-                        
-                    </form>
-                </div>
-            </div>
-            <div class="container scrollUpSec">
-                <h3 class="scrollTopBtn"><a href="#">Scroll up</a></h3>
+        <!-- My work -->
+        <div class="container mt-3 projects" id="projects">
+            <div class="row myProjects">
+                <?php include "inc/projects.php"; ?>
             </div>
         </div>
-    </main>
+        <!-- Contact details and form -->
+        <div class="container contact" id="contact">
+            <!-- Get in touch -->
+            <div class="col-sm-5 getintouch">
+                <h1>Get In Touch</h1>
+                <p>For any further information, please do not hesitate to call or email me using my details below:</p>
+                <p><span><i class="fa-solid fa-mobile-screen"></i></span><a class="telLink" href="tel:+447394705450">
+                        07394 705450</a></p>
+                <p><span><i class="fa-regular fa-envelope"></i></span><a class="emailLink"
+                        href="mailto:elizabeth.kodjo@yahoo.com"> ellizakodjo@outlook.com</a></p>
+            </div>
+            <!-- Contact Form -->
+            <div class="col-sm-7">
+                <form class="form" id="form" method="POST" action="inc/validateForm.php">
+                    <div class="mb-3 mt-3">
+                        <?php if (isset($_SESSION['status']) && $_SESSION['status'] === 'error'):
+                            $errors = $_SESSION['errors'];
+                            ?>
+                            <ul class="serverErrors">
 
-    <!-- footer -->
-   <?php include "inc/footer.php";
+                                <?php foreach ($errors as $e): ?>
+                                    <li><?= $e; ?></li>
+                                <?php endforeach; ?>
 
-   unset($_SESSION['status']);
-   unset($_SESSION['errors']);
-   unset($_SESSION['data']);
+                            </ul>
+                            <?php
+                        elseif (isset($_SESSION['status']) && $_SESSION['status'] === 'success'):
+                            $data = $_SESSION['data'];
+                            ?>
+                            <div class="serverSuccess">
+                                <p>Form successfully submitted</p>
+                            </div>
+                        <?php endif; ?>
+                        <div class="row formnames">
+                            <div class="col-sm-6 input-control">
+
+
+                                <label for="fname">First Name <small><em>*</em></small></label>
+                                <input type="text" class="form-control" id="fname" placeholder="First Name*"
+                                    name="fname">
+                                <small class="errorMsg"><?= $firstnameErr ?></small>
+
+
+                            </div>
+                            <div class="col-sm-6 input-control">
+                                <label for="lname">Last Name <small>*</small></label>
+                                <input type="text" class="form-control" id="lname" placeholder="Last Name*"
+                                    name="lname">
+                                <small class="errorMsg"><?= $lastnameErr ?></small>
+                            </div>
+                        </div>
+                        <div class="mb-3 input-control">
+                            <label for="email">Email <small>*</small></label>
+                            <input type="text" class="form-control" id="email" placeholder="Email Address" name="email"
+                                value="<?= $email ?>">
+                            <small class="errorMsg"><?= $emailErr ?></small>
+                        </div>
+                        <div class="mb-3 input-control">
+                            <label for="phone">Phone <small>*</small></label>
+                            <input type="tel" class="form-control" id="phone" placeholder="Phone number" name="phone"
+                                value="<?= $phone ?>">
+                            <small class="errorMsg"><?= $phoneErr ?></small>
+                        </div>
+                        <div class="mb-3 input-control">
+                            <label for="message">Message <small>*</small></label>
+                            <textarea class="form-control" rows="5" id="message" name="message"
+                                value="<?= $message ?>"></textarea>
+                            <small class="errorMsg"><?= $messageErr ?></small>
+                        </div>
+                        <input type="hidden" name="token" value="">
+                        <button type="submit" class="btn btn-primary submitbtn" name="submit">Submit</button>
+                        <p class="errorMsg"></p>
+
+                    </div>
+
+                </form>
+            </div>
+        </div>
+        <div class="container scrollUpSec">
+            <h3 class="scrollTopBtn"><a href="#">Scroll up</a></h3>
+        </div>
+    </div>
+</main>
+
+<!-- footer -->
+<?php include "inc/footer.php";
+
+unset($_SESSION['status']);
+unset($_SESSION['errors']);
+unset($_SESSION['data']);
